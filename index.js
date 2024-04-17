4f7509dd4247c4ecd8db738218c15d15e7761631
deep-sutariya <d33psutariya@gmail.com>
2024-04-17 14:42:28 +0530

    "Render"

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const connectDB = require('./db/db');
 const express = require('express');
 const cors = require('cors');
-import serverless from "serverless-http";
-import router from "./route/route";
+// import serverless from "serverless-http";
+// import router from "./route/route";
 
 const PORT = 5000;
 const app = express();
@@ -18,11 +18,11 @@ app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
 connectDB();
 
-api.use('/.netlify/functions/', router);
+// api.use('/.netlify/functions/', router);
 app.use(require('./route/route'))
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
 })
 
-export const handler = serverless(app);
+// export const handler = serverless(app);
